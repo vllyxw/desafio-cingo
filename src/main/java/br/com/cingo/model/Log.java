29781fecc786afff48effcf78ab2505f1879a1d7
vllyxw <victoriapansini661@gmail.com>
2025-03-06 13:36:49 -0300

    estruturando o model

--- src/main/java/br/com/cingo/model/Log.java
+++ src/main/java/br/com/cingo/model/Log.java
@@ -1,2 +1,17 @@
-package cingo.model;public class Log {
+package cingo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "log")
+public class Log {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "conteudo", nullable = false, unique = true)
+    private String conteudo;
+
+    @Column(name = "vezes", nullable = false)
+    private Long vezes;
 }
--- src/main/java/br/com/cingo/model/LogDAO.java
+++ src/main/java/br/com/cingo/model/LogDAO.java
@@ -1,2 +1,42 @@
-package cingo.model;public class LogDAO {
+package cingo.model;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import javax.persistence.Query;
+import java.util.List;
+
+public class LogDAO {
+    private SessionFactory sessionFactory;
+
+    public LogDAO() {
+        sessionFactory = HibernateUtil.getSessionFactory;
+    }
+
+    public void saveOrUpdate (Log log) {
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+
+        Query query = session.createQuery("FROM Log WHERE conteudo = :conteudo");
+        query.setParameter("conteudo", log.getConteudo());
+        Log existente = (Log) query.uniqueResult();
+
+        if (existente != null) {
+            existente.setVezes(existente.getVezes() + log.getVezes());
+            session.update(existente);
+        } else {
+            session.save(log);
+        }
+
+        tx.commit();
+        session.close();
+    }
+
+    public List<Log> logList() {
+        Session session = sessionFactory.openSession();
+        List<Log> logs = session.createQuery("FROM Log ORDER BY vezes DESC", Log.class).list();
+        session.close();
+        return logs;
+    }
 }
